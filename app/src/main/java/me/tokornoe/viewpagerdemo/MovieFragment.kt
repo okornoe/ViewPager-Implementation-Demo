92df31e676ad0dd4dddcdfb362958bba0e44baf9
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-30 22:41:22 +0000

    remove unneeded code

--- app/src/main/java/me/tokornoe/viewpagerdemo/MovieFragment.kt
+++ app/src/main/java/me/tokornoe/viewpagerdemo/MovieFragment.kt
@@ -37,23 +37,4 @@ class MovieFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_movie, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MovieFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MovieFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
