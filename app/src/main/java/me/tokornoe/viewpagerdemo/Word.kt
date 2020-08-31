721b87aa2e172fb4f850063d0757ad48b5a57e0e
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-31 09:42:27 +0000

    add word data class

---
+++ app/src/main/java/me/tokornoe/viewpagerdemo/Word.kt
@@ -0,0 +1,8 @@
+package me.tokornoe.viewpagerdemo
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
