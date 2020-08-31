d4c1e89a55d3b07eef8228d60ab3ddee9d7cd2c2
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-31 09:44:31 +0000

    add WordRoomDatabase

---
+++ app/src/main/java/me/tokornoe/viewpagerdemo/WordRoomDatabase.kt
@@ -0,0 +1,64 @@
+package me.tokornoe.viewpagerdemo
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Database(entities = [Word::class], version = 1, exportSchema = false)
+abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        //singleton prevents multiple instances of database opening at the same time
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).addCallback(WordDatabaseCallback(scope)).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+
+        private class WordDatabaseCallback(private val scope: CoroutineScope) :
+            RoomDatabase.Callback() {
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                INSTANCE?.let { database ->
+                    scope.launch {
+                        populateDatabase(database.wordDao())
+                    }
+                }
+            }
+
+            suspend fun populateDatabase(wordDao: WordDao) {
+                //Delete all content here
+                //wordDao.deleteAll()
+
+                // Add sample words
+                var word = Word("Kelvin")
+                wordDao.insert(word)
+                word = Word("Rose")
+                wordDao.insert(word)
+                word = Word("Rhoda")
+                wordDao.insert(word)
+                word = Word("Benjamin")
+                wordDao.insert(word)
+            }
+        }
+    }
+}
